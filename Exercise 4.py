7cccdbf517b12be25c3529db6aca85fbc08a1bce
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-08-30 18:52:17 +0300

    4.4 fixed

--- Exercise 4.py
+++ Exercise 4.py
@@ -33,15 +33,7 @@ print(f"The minimum number received from the user is {min(mylist)}")
 4.4
 import random
 random_number = random.randint(1, 10)
-guess_number = int(input("Enter the number between 1 and 10: "))
-if guess_number == random_number:
-    print(f"Nice, you guessed it right: {guess_number}")
-elif guess_number == random_number:
-    print(f"Nice, you guessed it right: {guess_number}")
-elif guess_number > random_number:
-    print("Oh, sorry, your guess is too high")
-elif guess_number < random_number:
-    print("Oh, sorry, your guess is too low")
+guess_number = 0
 while guess_number != random_number:
     guess_number = int(input("Enter the number between 1 and 10: "))
     if guess_number == random_number:
--- Just for fun.py
+++ Just for fun.py
@@ -1,21 +1,2 @@
-import random
-random_number = random.randint(1, 10)
-print(random_number)
-guess_number = int(input("Enter the number between 1 and 10: "))
-if guess_number == random_number:
-    print(f"Nice, you guessed it right: {guess_number}")
-elif guess_number == random_number:
-    print(f"Nice, you guessed it right: {guess_number}")
-elif guess_number > random_number:
-    print("Oh, sorry, your guess is too high")
-elif guess_number < random_number:
-    print("Oh, sorry, your guess is too low")
-
-    while guess_number != random_number:
-        guess_number = int(input("Enter the number between 1 and 10:"))
-        if guess_number == random_number:
-            print(f"Nice, you guessed it right: {guess_number}")
-        elif guess_number > random_number:
-            print("Oh, sorry, your guess is too high")
-        else:
-            print("Oh, sorry, your guess is too low")
+correct_username = "python"
+correct_password = "rules"
