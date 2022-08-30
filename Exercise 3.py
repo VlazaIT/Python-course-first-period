6f048f52b98743d5d5b6626742b4c78bfecea5cc
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-08-30 14:22:34 +0300

    Added 3.1

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,5 +1,9 @@
-Exercise 1
+3.1
 
-Hello
+zander_length = float(input("Write down the length of the zander in cm: "))
+if zander_length < 42:
+    print("Release the fish back into the lake since it is", 42 - zander_length, "cm below the size limit")
+else:
+    print("Zander's length is within the limit, you may keep the fish")
 
-Commit
+3.2
