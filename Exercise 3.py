32aa7995cb4c0e0db0d7df2cb2ecb1c81cca9f5a
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-08-30 14:53:22 +0300

    Added 3.2

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,5 +1,4 @@
 3.1
-
 zander_length = float(input("Write down the length of the zander in cm: "))
 if zander_length < 42:
     print("Release the fish back into the lake since it is", 42 - zander_length, "cm below the size limit")
@@ -7,3 +6,18 @@ else:
     print("Zander's length is within the limit, you may keep the fish")
 
 3.2
+cabin_class = str(input("Enter your cabin class: "))
+if cabin_class == "LUX":
+    print("Upper-deck cabin with a balcony")
+elif cabin_class == "A":
+    print("Above the car deck, equipped with a window")
+elif cabin_class == "B":
+    print("Windowless cabin above the car deck")
+elif cabin_class == "C":
+    print("Windowless cabin below the car deck")
+else:
+    print("Invalid cabin class")
+
+3.3
+
+
