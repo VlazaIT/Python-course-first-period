c335ac1fc256e123a6ebae7813f18f1b5e095d3e
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-08-30 15:35:53 +0300

    Added 3.3

--- Exercise 3.py
+++ Exercise 3.py
@@ -38,3 +38,10 @@ elif gender == "Female":
 else:
     print("Invalid biological gender entered")
 
+3.4
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
+
