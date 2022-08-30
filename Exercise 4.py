c79bea0cb9a301cc86c0c01ce7ec70fcf59fd2a2
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-08-30 16:54:43 +0300

    Added 4.2

--- Exercise 4.py
+++ Exercise 4.py
@@ -4,3 +4,11 @@ while number <= 1000:
     if number % 3 == 0:
         print(number)
     number = number + 1
+
+4.2
+inches = float(input("Enter an amount of inches: "))
+while inches >= 0:
+    cm = inches*2.54
+    print(f"There is {cm} cm in {inches} inches")
+    inches = float(input("Enter an amount of inches: "))
+print("Program finished due to a negative value")
