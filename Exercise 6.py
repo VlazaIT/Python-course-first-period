527eacac8e4f275a9991948798d6e26faea1cf98
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-09-06 12:39:21 +0300

    6.2 added

--- Exercise 6.py
+++ Exercise 6.py
@@ -1,3 +1,5 @@
+#6.1
+
 import random
 
 def roll():
@@ -12,3 +14,22 @@ while value != 6:
     print(f"The result of the {i} round is {value}")
 
 print(f"Since the result is 6, program is finished after {i} rounds")
+
+#6.2
+
+import random
+
+def roll(max_value):
+    value = random.randint(1,max_value)
+    return value
+
+value = 0
+max_value = int(input("Enter the number of sides on the dice: "))
+i = 0
+while value != max_value:
+    value = roll(max_value)
+    i = i + 1
+    print(f"The result of the {i} round is {value}")
+
+print(f"Since the result is {max_value}, program is finished after {i} rounds")
+
