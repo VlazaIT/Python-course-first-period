2bc504e940138de6c67088191e7fd3e86c82f0a6
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-09-08 19:30:17 +0300

    7.2 added

--- Exercise 7.py
+++ Exercise 7.py
@@ -15,3 +15,19 @@ elif number_of_month in (6,7,8):
 else:
     print(f"It is {season[2]} in that month.")
 
+#7.2
+
+names = set()
+new_name = str(input("Enter the name: "))
+
+while new_name != "":
+    if new_name in names:
+        print("Existing name")
+    else:
+        names.add(new_name)
+        print("New name")
+    new_name = str(input("Enter the name: "))
+
+else:
+    for n in names:
+        print(n)
